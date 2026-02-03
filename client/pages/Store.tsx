b6b33e68953df7c1aede9dcb8a7ff07fe4654b08
merfume-com <merfume.s@gmail.com>
2026-02-03 14:38:34 +0530

    Update Store.tsx

--- client/pages/Store.tsx
+++ client/pages/Store.tsx
@@ -1564,7 +1564,7 @@ interface Product {
 
 // Ek centralized axios instance create karte hain
 const api = axios.create({
-  baseURL: "https://e46b4bafada4.ngrok-free.app",
+  baseURL: "http://merfume.dpdns.org",
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': '69420'
