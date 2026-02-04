e75067b7df55bd1ad5243a565a92ba1dd04f98a7
merfume-com <merfume.s@gmail.com>
2026-02-04 20:39:02 +0530

    Update Contact.tsx

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -1521,7 +1521,8 @@ export default function Contact() {
     setFormData((prev) => ({ ...prev, inquiryType: value }));
   };
 
-  const API_BASE_URL = "https://e46b4bafada4.ngrok-free.app";//${API_BASE_URL}
+  // const API_BASE_URL = "https://e46b4bafada4.ngrok-free.app";//${API_BASE_URL}
+  const API_BASE_URL = "https://merfume-backend-production-5068.up.railway.app";
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
