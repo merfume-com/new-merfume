87597cd97ca28c0a13e2b11328c770c575eb9f94
merfume-com <merfume.s@gmail.com>
2026-02-04 19:02:40 +0530

    Update AdminDashboard.tsx

--- client/pages/AdminDashboard.tsx
+++ client/pages/AdminDashboard.tsx
@@ -5951,7 +5951,7 @@ const api = axios.create({
   baseURL: "https://e46b4bafada4.ngrok-free.app",
   headers: {
     "Content-Type": "application/json",
-    // "ngrok-skip-browser-warning": "69420",
+    "ngrok-skip-browser-warning": "69420",
     "Accept": "application/json",
   },
 });
