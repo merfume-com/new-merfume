1f4ba5381fc2c1e97f3718340ec42c3861c89747
merfume-com <merfume.s@gmail.com>
2026-02-04 18:49:21 +0530

    Update ProductCard.tsx

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1330,7 +1330,7 @@ interface ProductCardProps {
 
 // Axios instance create karte hain
 const api = axios.create({
-  baseURL: "http://merfume.dpdns.org",
+  baseURL: "https://e46b4bafada4.ngrok-free.app",
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': '69420'
